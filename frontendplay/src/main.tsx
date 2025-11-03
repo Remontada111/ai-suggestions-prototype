f058b83da5472ec10676e8a3a1665c4d49aeca91
AI Codegen <ai-codegen@users.noreply.github.com>
2025-11-03 18:57:22 +0100

    not working

--- frontendplay/src/main.tsx
+++ frontendplay/src/main.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-
+import Menud96dc2 from './components/ai/Menu-d96dc2';
+import Menufc7725 from './components/ai/Menu-fc7725';
 
 
 
@@ -33,6 +34,16 @@ function App() {
         }}
       >
         {/* AI-INJECT-MOUNT:BEGIN */}
+        <div id="__AI_MOUNT_GRID__" className="flex flex-wrap gap-4 items-start">
+        
+        <> {/* AI-TILE:./components/ai/Menu-d96dc2:BEGIN */}
+        <div className="relative w-[1280px] h-[800px] overflow-hidden rounded-md ring-1 ring-black/10 bg-white">
+        <Menud96dc2 />
+        </div> {/* AI-TILE:./components/ai/Menu-d96dc2:END */}</>
+        <> {/* AI-TILE:./components/ai/Menu-fc7725:BEGIN */}
+        <div className="relative w-[1280px] h-[800px] overflow-hidden rounded-md ring-1 ring-black/10 bg-white">
+        <Menufc7725 />
+        </div> {/* AI-TILE:./components/ai/Menu-fc7725:END */}</></div>
         {/* AI-INJECT-MOUNT:END */}</div>
     </div>
   );
